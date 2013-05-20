a2e3eb804f39d179fbab0cd2f152c5249207796a
quach hoang <quachhoang_2005@yahoo.com>
2013-05-20 16:16:58 +0700

    Create mini-style-www.savingtours.com.js

---
+++ mini-style-www.savingtours.com.js
@@ -0,0 +1,25 @@
+.top_index{padding-bottom:20px}
+.row_index{width:330px}
+.left_{float:left}
+.right_{float:right}
+.row_index_tour{
+background:url(http://www.savingtours.com/images/line1.png) no-repeat bottom left,url(http://www.savingtours.com/images/line2.png) no-repeat right bottom;
+-pie-background:url(http://www.savingtours.com/images/line1.png) no-repeat bottom left,url(http://www.savingtours.com/images/line2.png) no-repeat right bottom;
+position:relative;
+padding:0px 25px 25px 0px
+}
+.div_name_index{
+padding-bottom:15px;
+font-size:16px;
+font-family:Tahoma
+}
+.div_img_index{position:relative}
+.img_index{width:294px;height:160px;
+-moz-border-radius:5px;
+border-radius:5px;
+position:relative;
+border:5px solid #fff
+}
+.summary_index{text-align:justify;padding:10px 0px 15px 0px}
+.view_more_index{text-align:right;background:url(http://www.savingtours.com/images/icon_viewmore.png) no-repeat right center}
+.a_view_more{text-decoration:underline;padding-right:10px;color:#8d7035;font-size:11px;font-family:tahoma}
